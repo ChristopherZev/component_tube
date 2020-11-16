9224561089d03f55e060b9a5b88d553ccdbc9bd5
Christopher Zevallos <christopher_zev@icloud.com>
2020-11-16 16:12:19 -0500

    updated API file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
+// Omit this file making a change
+// const APIKEY = 'AIzaSyA41OopxiMLIkA_ZbbnYQHCYXmZ_Oj2pI4';
 
-const APIKEY = process.env.APIKEY
+const APIKEY= process.env.APIKEY
+
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: `${APIKEY}`
+        key: `${proccess.env.APIKEY}`
     }
 });
